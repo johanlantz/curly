5b314a8b5a8ce35619b53ceaef455a379b107a47
Johan <johan@tid.es>
2016-04-08 13:42:19 +0200

    prebuilt static libs and scripts for android

--- third-party/curl/android/README.txt
+++ third-party/curl/android/README.txt
@@ -1,6 +1,3 @@
-This repo contains a nice build script for libcurl on Android as well as prebuilt libs:
+This folder contains prebuilt static curl libraries (with ssl support) for Android.
 
-https://github.com/gcesarmza/curl-android-ios/tree/master/prebuilt-with-ssl/android
-
-Do note that you might want to exclude protocols that you are not interested in 
-by adding params such as —no-pop3 etc in the .sh file.
+You need to link with the corresponding openssl libraries.
---
+++ third-party/manual_build/build_scripts/jni/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH := $(call my-dir)
+
+#SSL
+include $(LOCAL_PATH)/ssl.mk
+  
+#Crypto
+include $(LOCAL_PATH)/crypto.mk
+                    
+#cURL
+include $(LOCAL_PATH)/curl.mk
+
+#Static libssl
+include $(CLEAR_VARS)
+LOCAL_MODULE := ssl
+LOCAL_SRC_FILES := $(SSL_LOCAL_SRC_FILES)
+LOCAL_C_INCLUDES := $(SSL_LOCAL_C_INCLUDES)
+LOCAL_CPPFLAGS += -std=gnu++0x
+include $(LOCAL_PATH)/optimizations.mk
+LOCAL_CFLAGS += $(SSL_COMMON_CFLAGS)
+include $(BUILD_STATIC_LIBRARY)
+
+#Static libcrypto
+include $(CLEAR_VARS)
+LOCAL_MODULE := crypto
+LOCAL_SRC_FILES := $(CRYPTO_LOCAL_SRC_FILES)
+LOCAL_C_INCLUDES := $(CRYPTO_LOCAL_C_INCLUDES)
+LOCAL_CPPFLAGS += -std=gnu++0x
+include $(LOCAL_PATH)/optimizations.mk
+LOCAL_CFLAGS += $(CRYPTO_COMMON_CFLAGS)
+include $(BUILD_STATIC_LIBRARY)
+
+#Static libcurl
+include $(CLEAR_VARS)
+LOCAL_MODULE := curl
+#LOCAL_SRC_FILES := $(SSL_LOCAL_SRC_FILES) $(CRYPTO_LOCAL_SRC_FILES) $(CURL_LOCAL_SRC_FILES)
+LOCAL_SRC_FILES := $(CURL_LOCAL_SRC_FILES)
+LOCAL_C_INCLUDES := $(SSL_LOCAL_C_INCLUDES) $(CRYPTO_LOCAL_C_INCLUDES) $(CURL_LOCAL_C_INCLUDES)
+LOCAL_CPPFLAGS += -std=gnu++0x
+include $(LOCAL_PATH)/optimizations.mk
+LOCAL_CFLAGS += $(CURL_COMMON_CFLAGS)
+include $(BUILD_STATIC_LIBRARY)
