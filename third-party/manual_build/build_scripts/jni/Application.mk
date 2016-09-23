a1a2dc877d19e7aaa90380294b4c269aa86ddb97
Johan <johan@tid.es>
2016-09-23 14:28:04 +0200

    Use shared c++ runtime

--- third-party/manual_build/build_scripts/jni/Application.mk
+++ third-party/manual_build/build_scripts/jni/Application.mk
@@ -1,6 +1,7 @@
 APP_OPTIM := release
 APP_PLATFORM := android-8
-APP_STL := gnustl_static
+#APP_STL := gnustl_static
+APP_STL := c++_shared
 APP_CPPFLAGS += -frtti
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -DANDROID
